0551168a7e9fef23b1c56fa334745957f42938dc
MonkeJacquis <24CurryJohn@bprep.org>
2021-10-07 17:18:44 -0700

    wrote button drive

---
+++ src/main/java/frc/robot/commands/XboxButtonDrive.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import frc.robot.subsystems.DriveTrain;
+
+import static frc.robot.Constants.OIConstants.*;
+
+public class XboxButtonDrive extends CommandBase {
+ 
+  private final DriveTrain myDriveTrain;
+
+  private final XboxController driverCont;
+  /** Creates a new XboxButtonDrive. */
+  public XboxButtonDrive(DriveTrain driveTrain) {
+
+      driverCont = new XboxController(driverContPort);
+
+      myDriveTrain = driveTrain; 
+
+      addRequirements(myDriveTrain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {// Called once the command ends or is interrupted.
+    if(driverCont.getAButton()){
+    
+      myDriveTrain.driveL(.2);
+      myDriveTrain.driveR(.2);
+    }else if(driverCont.getYButton()){
+      myDriveTrain.driveL(-.2);
+      myDriveTrain.driveR(-.2);
+    }else{
+      myDriveTrain.driveL(0);
+    }
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
