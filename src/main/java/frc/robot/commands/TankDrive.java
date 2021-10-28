a268e028838ef56e0c6a0161e968b8a94c371d50
een39 <24stoddardian@bprep.org>
2021-10-28 16:09:01 -0700

    Create TankDrive.java

---
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import frc.robot.subsystems.DriveTrain;
+
+import static frc.robot.Constants.OIConstants.*;
+
+public class TankDrive extends CommandBase {
+  /** Creates a new TankDrive. */
+  private final DriveTrain myDriveTrain;
+
+  private final XboxController driverCont;
+
+  public TankDrive(DriveTrain driveTrain) {
+    driverCont = new XboxController(driverContPort);
+
+    myDriveTrain = driveTrain;
+
+    addRequirements(myDriveTrain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    myDriveTrain.driveR(driverCont.getY(Hand.kRight));
+   
+    }
+  
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
