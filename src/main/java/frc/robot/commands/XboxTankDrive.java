8b371bc83ec0145bbdd1ac818a5778e40b9c459d
juliashimshock <24shimshockjulia@bprep.org>
2021-10-28 16:23:13 -0700

    Update XboxTankDrive.java

--- src/main/java/frc/robot/commands/XboxTankDrive.java
+++ src/main/java/frc/robot/commands/XboxTankDrive.java
@@ -1,58 +1,45 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.subsystems.DriveTrain;
 
 import static frc.robot.Constants.OIConstants.*;
-
+/** Add your docs here. */
 public class XboxTankDrive extends CommandBase {
+    private final DriveTrain myDriveTrain;
 
-  private final DriveTrain myDriveTrain;
+    private final XboxController driverCont;
 
-  private final XboxController driverCont;
+    public XboxTankDrive(DriveTrain driveTrain) {
 
-  public XboxTankDrive(DriveTrain driveTrain) {
+        driverCont = new XboxController(driverContPort);
+        
+        myDriveTrain = driveTrain;
 
-    driverCont = new XboxController(driverContPort);
+        addRequirements(myDriveTrain);
 
-    myDriveTrain = driveTrain;
+        }
 
-    addRequirements(myDriveTrain); // Use addRequirements() here to declare subsystem dependencies.
-  }
+@Override
+public void initialize() {}
 
-  @Override
-  public void initialize() {   // Called when the command is initially scheduled.
-  }
+@Override
+public void execute() {
+    myDriveTrain.driveL((driverCont.getY(Hand.kLeft)));
+    myDriveTrain.driveR((driverCont.getY(Hand.kRight)));    
+}
 
-  @Override
-  public void execute() {   // Called every time the scheduler runs while the command is scheduled.
-    if(Math.abs(driverCont.getY(Hand.kRight)) >= .125){
-    	myDriveTrain.driveR(-1 * driverCont.getY(Hand.kRight) * 1.0);
-    }else{
-    	myDriveTrain.driveR(0);
-    }
-    if(Math.abs(driverCont.getY(Hand.kLeft)) >= .125){
-      myDriveTrain.driveL(-1 * driverCont.getY(Hand.kLeft) * 1.0);
-    }else{
-    	myDriveTrain.driveL(0);
-    }
-  }
+@Override
+public void end(boolean interrupted) {}
 
-  @Override
-  public void end(boolean interrupted) {   // Called once the command ends or is interrupted.
-  }
-
-  @Override
-  public boolean isFinished() {  // Returns true when the command should end.
+@Override
+public boolean isFinished() {
     return false;
-  }
+}
 }
