7d2eb7fa45a8633e91d2470f71f06d2a8719145e
KevinLiu234 <25liukevin@bprep.org>
2021-11-04 16:54:27 -0700

    Update XboxTankDrive.java

--- src/main/java/frc/robot/commands/XboxTankDrive.java
+++ src/main/java/frc/robot/commands/XboxTankDrive.java
@@ -7,52 +7,48 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.subsystems.DriveTrain;
 
 import static frc.robot.Constants.OIConstants.*;
 
 public class XboxTankDrive extends CommandBase {
-
-  private final DriveTrain myDriveTrain;
-
-  private final XboxController driverCont;
-
-  public XboxTankDrive(DriveTrain driveTrain) {
-
-    driverCont = new XboxController(driverContPort);
-
-    myDriveTrain = driveTrain;
-
-    addRequirements(myDriveTrain); // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  @Override
-  public void initialize() {   // Called when the command is initially scheduled.
-  }
-
-  @Override
-  public void execute() {   // Called every time the scheduler runs while the command is scheduled.
-    if(Math.abs(driverCont.getY(Hand.kRight)) >= .125){
-    	myDriveTrain.driveR(-1 * driverCont.getY(Hand.kRight) * 1.0);
-    }else{
-    	myDriveTrain.driveR(0);
+  
+    private final DriveTrain myDriveTrain;
+  
+    private final XboxController driverCont;
+  
+    public XboxTankDrive(DriveTrain driveTrain) {
+  
+      driverCont = new XboxController(driverContPort);
+  
+      myDriveTrain = driveTrain;
+  
+      addRequirements(myDriveTrain);
     }
-    if(Math.abs(driverCont.getY(Hand.kLeft)) >= .125){
-      myDriveTrain.driveL(-1 * driverCont.getY(Hand.kLeft) * 1.0);
-    }else{
-    	myDriveTrain.driveL(0);
-    }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
   }
 
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void end(boolean interrupted) {   // Called once the command ends or is interrupted.
+  public void execute() {
+    myDriveTrain.driveL(driverCont.getY(Hand.kLeft));
+    myDriveTrain.driveR(driverCont.getY(Hand.kRight));
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
   }
 
+  // Returns true when the command should end.
   @Override
-  public boolean isFinished() {  // Returns true when the command should end.
+  public boolean isFinished() {
     return false;
   }
 }
