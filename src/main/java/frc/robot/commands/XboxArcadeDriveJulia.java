f0c7b042827af005c24c0a8a0e8224ba280ef159
juliashimshock <24shimshockjulia@bprep.org>
2021-11-04 18:03:14 -0700

    start of arcade drive

--- src/main/java/frc/robot/commands/XboxArcadeDriveJulia.java
+++ src/main/java/frc/robot/commands/XboxArcadeDriveJulia.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
@@ -12,41 +13,37 @@ import frc.robot.subsystems.DriveTrain;
 
 import static frc.robot.Constants.OIConstants.*;
 /** Add your docs here. */
-public class XboxTankDrive extends CommandBase {
+public class XboxArcadeDriveJulia {
     private final DriveTrain myDriveTrain;
 
     private final XboxController driverCont;
 
-    public XboxTankDrive(DriveTrain driveTrain) {
+    public XboxArcadeDriveJulia(DriveTrain driveTrain) {
 
         driverCont = new XboxController(driverContPort);
-        
+
         myDriveTrain = driveTrain;
 
         addRequirements(myDriveTrain);
-
-        }
-
+    }
 @Override
 public void initialize() {}
 
 @Override
 public void execute() {
-    if(Math.abs(driverCont.getY(Hand.kRight)) >= xboxDeadzone ) {
-        myDriveTrain.driveR((-1 * driverCont.getY(Hand.kRight))); 
-    }
-    if(Math.abs(driverCont.getY(Hand.kLeft)) >= xboxDeadzone ) {
-        myDriveTrain.driveL((-1 * driverCont.getY(Hand.kLeft)));
-    } 
-    if(Math.abs(driverCont.getY(Hand.kRight)) < xboxDeadzone ) {
-        myDriveTrain.driveR((0 * driverCont.getY(Hand.kRight)));
-    }
-    if(Math.abs(driverCont.getY(Hand.kLeft)) < xboxDeadzone ) {
-        myDriveTrain.driveL((0 * driverCont.getY(Hand.kLeft)));
-    }
-   SmartDashboard.getNumber("Xbox Left Stick Y", driverCont.getY());
-}
+    myDriveTrain.driveL(-1 * driverCont.getY(Hand.kLeft));
+    myDriveTrain.driveR(-1 * driverCont.getY(Hand.kLeft));
 
+    if(driverCont.getX(Hand.kLeft) > 0) {
+        myDriveTrain.driveR(-1 * driverCont.getX(Hand.kLeft));
+        myDriveTrain.driveL(0);
+    
+    }
+    if(driverCont.getX(Hand.kLeft) < 0 ) {
+        myDriveTrain.driveL(-1 * driverCont.getX(Hand.kLeft));
+        myDriveTrain.driveR(0);
+    }
+}
 @Override
 public void end(boolean interrupted) {}
 
